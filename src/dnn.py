ac06507e9137610135f44345f8ab734c385e79e7
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-05-21 23:33:17 -0400

    HUGE - this is performing extremely well

--- src/dnn.py
+++ src/dnn.py
@@ -1,16 +1,23 @@
+from datetime import datetime, timedelta
+import logging, os
+from alpaca_trade_api.rest import TimeFrame
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 # just a test right now
 from keras import optimizers
 from keras.engine import training
 from channels.alpaca import Alpaca
 import pandas as pd, numpy as np
 from finta import TA as ta
-import logging, os
 from tqdm import trange, tqdm
 from sklearn import preprocessing
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam, RMSprop
+from sklearn.model_selection import train_test_split
+
+pd.options.mode.chained_assignment = None  # default='warn'
 
 os.environ["NUMEXPR_MAX_THREADS"] = "8"
 
@@ -23,25 +30,14 @@ logger = logging.getLogger(__name__)
 class FEDNN:  # feature engineering deep neural network
     def __init__(
         self,
-        dfcols: dict = {
-            "close": "close",
-            "open": "open",
-            "low": "low",
-            "high": "high",
-            "volume": "volume",
-        },
         mv_avgs: list = [5, 10, 20, 30, 50, 100, 200],
         units: int = 32,
         learning_rate: float = 0.0001,
         lags: int = 5,
-        epochs=25,
+        cols: list = [],
+        epochs: int = 25,
     ) -> None:
         """dfcols is the labels for the dataframe that you recieve"""
-        self.close = dfcols["close"]
-        self.open = dfcols["open"]
-        self.low = dfcols["low"]
-        self.high = dfcols["high"]
-        self.volume = dfcols["volume"]
         self.mv_avgs = mv_avgs
 
         # for ml
@@ -49,13 +45,18 @@ class FEDNN:  # feature engineering deep neural network
         self.optimizer = Adam(learning_rate=learning_rate)
 
         self.lags = lags  # number of lags
-        self.cols = [f"lag_{l}" for l in range(1, self.lags + 1)]  # col labels
+        self.cols = cols  # col labels
         self.epochs = epochs
+
+    def _add_columns(self, *args):
+        """Add columns to self.columns"""
+        self.cols.extend(list(args))
+        self.cols = list(set(self.cols))  # remove duplicates
 
     def _create_returns(self, data: pd.DataFrame):
         """Create the log returns"""
         try:
-            data["return"] = np.log(data[self.close] / data[self.close].shift(1))
+            data["return"] = np.log(data["close"] / data["close"].shift(1))
         except Exception as e:
             logger.warning(f"Unknown error occured while generating returns ({e})")
         return data
@@ -74,6 +75,7 @@ class FEDNN:  # feature engineering deep neural network
             for lag in range(1, self.lags + 1):
                 col = f"lag_{lag}"
                 data[col] = data["return"].shift(lag)
+                self.cols.append(col)
             data.dropna(inplace=True)
         except Exception as e:
             logger.warning(f"Unknown error occured while generating lags ({e})")
@@ -90,7 +92,6 @@ class FEDNN:  # feature engineering deep neural network
         # create copy
         df = data.copy()
         try:
-            """
             ### Oscillators ###
             ## RSI
             df = self._append_data(df, ta.RSI(df))
@@ -98,10 +99,6 @@ class FEDNN:  # feature engineering deep neural network
             df = self._append_data(df, ta.STOCH(df))
             ## CCI
             df = self._append_data(df, ta.CCI(df))
-            ## ADX
-            df = self._append_data(df, ta.ADX(df))
-            ## DMI (Added to aid in interpreting ADX)
-            df = self._append_data(df, ta.DMI(df, 14))
             ## Awesome
             df = self._append_data(df, ta.AO(df))
             ## Momentum
@@ -114,8 +111,6 @@ class FEDNN:  # feature engineering deep neural network
             df = self._append_data(df, ta.STOCHRSI(df))
             ## Williams %R
             df = self._append_data(df, ta.WILLIAMS(df))
-            ## Bull-Bear Power
-            df = self._append_data(df, ta.EBBP(df))
             ## Ultimate (FinTA does not name this column, so we must)
             df = self._append_data(df, ta.UO(df), ["UO"])
 
@@ -131,16 +126,13 @@ class FEDNN:  # feature engineering deep neural network
             df = self._append_data(
                 df, ta.ICHIMOKU(df).drop(["senkou_span_a", "SENKOU", "CHIKOU"], axis=1)
             )
-            """
             ### Custom
             df["momentum"] = df["return"].rolling(5).mean().shift(1)
+            df["volume_ma"] = df["volume"].rolling(5).mean().shift(1)
             df["volatility"] = df["return"].rolling(20).std().shift(1)
-            df["distance"] = (df[self.close] - df[self.close].rolling(50).mean()).shift(
-                1
-            )
+            df["distance"] = (df["close"] - df["close"].rolling(50).mean()).shift(1)
 
-            self.cols.extend(["momentum", "volatility", "distance"])
-            self.cols = list(set(self.cols))  # remove duplicates
+            self._add_columns("momentum", "volatility", "distance", "14 period RSI")
             ### drop na
             df.dropna(inplace=True)
         except Exception as e:
@@ -150,11 +142,12 @@ class FEDNN:  # feature engineering deep neural network
     def prime_data(self, data: pd.DataFrame, prune: bool = False):
         """Prime the data for the network"""
         if prune:
-            data = data[[self.close]]
+            data = data[["close"]]
         data = self._create_returns(data)
         data = self._create_direction(data)
         data = self._create_features(data)
         data = self._create_lags(data)
+        self.cols = list(set(self.cols))  # remove duplicates
         return data
 
     def fit_scaler(self, data: pd.DataFrame):
@@ -169,7 +162,7 @@ class FEDNN:  # feature engineering deep neural network
 
     def build(self, data: pd.DataFrame, summary: bool = False):
         """Compile model"""
-        print(self.cols)
+        logger.info(f"Using features {self.cols}")
         self.fit_scaler(data)
         self.model = Sequential()
         self.model.add(
@@ -182,7 +175,6 @@ class FEDNN:  # feature engineering deep neural network
         )
         if summary:
             self.model.summary()
-        return self.model
 
     def train(self, data: pd.DataFrame):
         """Train the model"""
@@ -194,23 +186,47 @@ class FEDNN:  # feature engineering deep neural network
             verbose=False,
         )
         self.model.evaluate(data_normalized[self.cols], data["direction"])
-        return self.model
+
+    def predict(self, data: pd.DataFrame):
+        """Predict"""
+        data_normalized = self.normalize(data)
+        pred = np.where(self.model.predict(data_normalized[self.cols]) > 0.5, 1, 0)
+        data["prediction"] = np.where(pred > 0, 1, -1)
+        data["strategy"] = data["prediction"] * data["return"]
+        return data
+
+    def evaluate(self, data: pd.DataFrame):
+        """Vectorize evaluate"""
+        logger.info(f"Returns:\n{data[['return', 'strategy']].sum().apply(np.exp)}")
+        data[["return", "strategy"]].cumsum().apply(np.exp).plot(figsize=(10, 6))
 
 
 if __name__ == "__main__":
     ape = Alpaca()
 
-    symbol = "pg"
-    data = ape.get_bars(symbol)
+    symbol = "aal"
+    data = ape.get_bars(
+        symbol,
+        timeframe=TimeFrame.Minute,
+        start_time=datetime.now() - timedelta(days=14),
+    )
+
     logger.info(f"Got data for {symbol}")
 
-    fednn = FEDNN()
+    fednn = FEDNN(cols=["return"])
     # prime data
     data = fednn.prime_data(data)
+    # split
+    tt_split = 0.8
+    train, test = (
+        data[: int(data.shape[0] * tt_split)],
+        data[int(data.shape[0] * tt_split) :],
+    )
     # make model
-    fednn.build(data, summary=True)
+    fednn.build(train)
     # train model
-    fednn.train(data)
+    fednn.train(train)
     # predict
-    # p = fednn.predict(data)
-    # print(p)
+    predictions = fednn.predict(test)
+    # evaluate
+    fednn.evaluate(predictions)
